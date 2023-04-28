3f90471b9d284aae194b86a4def5eff5ae63d106
Anastasia <anastasiya_epam1@copyright.com>
2023-04-28 16:33:02 +0200

    added integraion tests

---
+++ src/test/java/jmp/amarchuk/DAO/UserDaoDBImplTest.java
@@ -0,0 +1,64 @@
+package jmp.amarchuk.DAO;
+
+
+import jmp.amarchuk.facade.BookingFacadeImpl;
+import jmp.amarchuk.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath:configurationTest.xml"})
+class UserDaoDBImplTest {
+
+    @Autowired
+    private UserDaoDBImpl userDao;
+    @Autowired
+    private BookingFacadeImpl bookingFacade;
+
+
+    List<User> list = new ArrayList<>();
+    private static Logger logger = LoggerFactory.getLogger(UserDaoDBImplTest.class);
+
+    @Test
+    void getAllUsers() {
+        int size=bookingFacade.getAllUsers().size();
+        assertEquals(6, size);
+        logger.info("GetAllUsers test passed!");
+
+    }
+
+    @Test
+    void createUser() {
+        User user=new User(9,"newStacy9","new_stacy@gmail.com");
+        bookingFacade.createUser(user);
+        assertEquals(7, userDao.sizeUsers());
+        logger.info("CreateUser test passed!");
+    }
+
+    @Test
+    void getUsersByName() {
+        User user10=new User(10,"New Name","new_name@gmail.com");
+        User user11=new User(11,"New Name","new_name@gmail.com");
+        list.add(user10);
+        list.add(user11);
+        bookingFacade.createUser(user10);
+        bookingFacade.createUser(user11);
+        list=bookingFacade.getUsersByName("New Name",1,1);
+        assertEquals(list.size(), list.size());
+        logger.info("GetUsersByName test passed!");
+    }
+
+
+
+}
